30b1a3ce7c3372d993e9b7ab024b3b4caae7d5b7
sauceSC <dayr0802@gmail.com>
2022-08-15 12:08:11 +0600

    Initial commit

---
+++ app/src/main/java/com/example/testbustracker/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testbustracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
